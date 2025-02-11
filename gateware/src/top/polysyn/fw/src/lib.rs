e91ca7383cbff106498eab81a88e2433632baf10
Sebastian Holzapfel <me@sebholzapfel.com>
2025-02-11 20:05:40 +0100

    hal: migrate impl_tiliqua_soc_pac to all cores

--- gateware/src/top/polysyn/fw/src/lib.rs
+++ gateware/src/top/polysyn/fw/src/lib.rs
@@ -6,33 +6,11 @@ pub use tiliqua_hal as hal;
 
 use tiliqua_lib::generated_constants::N_VOICES;
 
-tiliqua_hal::impl_serial! {
-    Serial0: pac::UART0,
-}
-
-tiliqua_hal::impl_timer! {
-    Timer0: pac::TIMER0,
-}
-
-tiliqua_hal::impl_i2c! {
-    I2c0: pac::I2C0,
-}
-
-tiliqua_hal::impl_encoder! {
-    Encoder0: pac::ENCODER0,
-}
-
-tiliqua_hal::impl_eurorack_pmod! {
-    EurorackPmod0: pac::PMOD0_PERIPH,
-}
+hal::impl_tiliqua_soc_pac!();
 
 tiliqua_hal::impl_polysynth! {
     Polysynth0: pac::SYNTH_PERIPH,
     N_VOICES
-}
-
-tiliqua_hal::impl_video! {
-    Video0: pac::VIDEO_PERIPH,
 }
 
 pub mod handlers;
