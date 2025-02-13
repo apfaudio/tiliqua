87ded0a7d7e66639d5682c8b9fae80f00f516a03
Sebastian Holzapfel <me@sebholzapfel.com>
2025-02-13 20:27:55 +0100

    floatoption

--- gateware/src/rs/lib/src/opt.rs
+++ gateware/src/rs/lib/src/opt.rs
@@ -318,3 +318,115 @@ where
         T::iter().count()
     }
 }
+
+#[derive(Clone, Copy)]
+pub enum FloatFormat {
+    Precision(u8),  // Display as float with N decimal places
+    Percent(u8),    // Display as percentage with N decimal places
+}
+
+#[derive(Clone)]
+pub struct FloatOption<T: FloatOptionConfig> {
+    name: &'static str,
+    pub value: T::Value,
+}
+
+pub trait FloatOptionConfig {
+    type Value: Copy + Default;
+    const STEP: Self::Value;
+    const MIN: Self::Value;
+    const MAX: Self::Value;
+    const FORMAT: FloatFormat;
+}
+
+impl<T: FloatOptionConfig> FloatOption<T> {
+    pub fn new(name: &'static str, value: T::Value) -> Self {
+        Self {
+            name,
+            value,
+        }
+    }
+}
+
+impl<T: FloatOptionConfig> OptionTrait for FloatOption<T>
+where
+    T::Value: Copy
+        + Default
+        + core::ops::Add<Output = T::Value>
+        + core::ops::Sub<Output = T::Value>
+        + core::ops::Div<Output = T::Value>
+        + core::ops::Mul<Output = T::Value>
+        + core::cmp::PartialOrd
+        + core::fmt::Display,
+    f32: From<T::Value>,
+    T::Value: From<f32>,
+{
+    fn name(&self) -> &'static str {
+        self.name
+    }
+
+    fn value(&self) -> OptionString {
+        let mut s: OptionString = String::new();
+        match T::FORMAT {
+            FloatFormat::Precision(precision) => {
+                write!(&mut s, "{:.*}", precision as usize, self.value).ok();
+            }
+            FloatFormat::Percent(precision) => {
+                let percent_value = f32::from(self.value) * 100.0;
+                write!(&mut s, "{:.*}%", precision as usize, percent_value).ok();
+            }
+        }
+        s
+    }
+
+    fn tick_up(&mut self) {
+        let new_value = self.value + T::STEP;
+        if new_value <= T::MAX {
+            self.value = new_value;
+        }
+    }
+
+    fn tick_down(&mut self) {
+        let new_value = self.value - T::STEP;
+        if new_value >= T::MIN {
+            self.value = new_value;
+        }
+    }
+
+    fn percent(&self) -> f32 {
+        let range = T::MAX - T::MIN;
+        let value = self.value - T::MIN;
+        f32::from(value) / f32::from(range)
+    }
+
+    fn n_unique_values(&self) -> usize {
+        let range = T::MAX - T::MIN;
+        let steps = range / T::STEP;
+        f32::from(steps) as usize + 1
+    }
+}
+
+// Macro for creating float option configs
+#[macro_export]
+macro_rules! float_option_config {
+    ($name:ident<$t:ty> { step: $step:expr, min: $min:expr, max: $max:expr, format: $format:expr }) => {
+        #[derive(Clone)]
+        pub struct $name;
+
+        impl FloatOptionConfig for $name {
+            type Value = $t;
+            const STEP: Self::Value = $step;
+            const MIN: Self::Value = $min;
+            const MAX: Self::Value = $max;
+            const FORMAT: FloatFormat = $format;
+        }
+    };
+    ($name:ident: $t:ty => $step:expr, $min:expr, $max:expr, $format:expr) => {
+        float_option_config!($name<$t> {
+            step: $step,
+            min: $min,
+            max: $max,
+            format: $format
+        });
+    };
+}
