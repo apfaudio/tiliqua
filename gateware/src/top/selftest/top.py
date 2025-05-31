ed50a5b47a84b98c9158221c867e93145b0da74b
Sebastian Holzapfel <me@sebholzapfel.com>
2025-05-31 18:14:47 +0200

    top/selftest: add USBSpeedTestDevice and scripts/speed_test from LUNA

--- gateware/src/top/selftest/top.py
+++ gateware/src/top/selftest/top.py
@@ -9,11 +9,28 @@ hardware issues and for bringup.
 
 import os
 
+from amaranth                    import *
+
 from tiliqua.tiliqua_soc         import TiliquaSoc
 from tiliqua.cli                 import top_level_cli
 
+from luna.gateware.applets.speed_test import USBSpeedTestDevice, VENDOR_ID, PRODUCT_ID
+
 class SelftestSoc(TiliquaSoc):
     brief = "Test & calibration utilities"
+
+    def elaborate(self, platform):
+
+        m = Module()
+
+        m.submodules += super().elaborate(platform)
+
+        m.submodules += USBSpeedTestDevice(generate_clocks=False,
+                                           phy_name=platform.default_usb_connection,
+                                           vid=VENDOR_ID,
+                                           pid=PRODUCT_ID)
+
+        return m
 
 if __name__ == "__main__":
     this_path = os.path.dirname(os.path.realpath(__file__))
