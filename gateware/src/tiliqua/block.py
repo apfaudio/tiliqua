4f04089bd91ecf941071cfad0b5bdc916f961a0e
Sebastian Holzapfel <me@sebholzapfel.com>
2025-07-12 20:05:19 +0200

    spectro: simple WrapCore

--- gateware/src/tiliqua/block.py
+++ gateware/src/tiliqua/block.py
@@ -6,7 +6,8 @@
 """Utilities for dealing with contiguous blocks of samples."""
 
 from amaranth import *
-from amaranth.lib import data
+from amaranth.lib import data, wiring, stream, fifo
+from amaranth.lib.wiring import In, Out
 
 class Block(data.StructLayout):
     """:class:`data.StructLayout` representing a 'Block' of samples.
@@ -55,3 +56,59 @@ def connect_without_payload(m, stream_o, stream_i):
         assert isinstance(shape_o, Block) and isinstance(shape_i, Block)
         m.d.comb += stream_i.payload.first.eq(stream_o.payload.first)
 
+class WrapCore(wiring.Component):
+
+    """
+    Promote any DSP component with simple i/o streams into supporting
+    :class:`Block` streams (tracking 'first' across i/o samples).
+
+    This only supports simple cores that have:
+    - An input stream 'i' with signature stream.Signature(shape)
+    - An output stream 'o' with signature stream.Signature(shape)
+    """
+
+    def __init__(self, core, max_latency=16):
+        self.core = core
+        self.shape = core.i.payload.shape()
+        self.max_latency = max_latency
+        super().__init__({
+            "i": In(stream.Signature(Block(self.shape))),
+            "o": Out(stream.Signature(Block(self.shape))),
+        })
+
+    def elaborate(self, platform):
+        m = Module()
+
+        m.submodules.dsp_core = dsp_core = self.core
+
+        # FIFO to preserve the 'first' signal
+        m.submodules.first_fifo = first_fifo = fifo.SyncFIFOBuffered(
+            width=1, depth=self.max_latency
+        )
+
+        sample_in = stream.Signature(self.shape).create()
+        m.d.comb += [
+            sample_in.valid.eq(self.i.valid),
+            sample_in.payload.eq(self.i.payload.sample),
+            self.i.ready.eq(sample_in.ready & first_fifo.w_rdy),
+        ]
+        wiring.connect(m, sample_in, dsp_core.i)
+
+        # Store 'first' signal in FIFO whenever a sample is transferred
+        m.d.comb += [
+            first_fifo.w_en.eq(self.i.valid & self.i.ready),
+            first_fifo.w_data.eq(self.i.payload.first),
+        ]
+
+        sample_out = stream.Signature(self.shape).flip().create()
+        wiring.connect(m, dsp_core.o, sample_out)
+
+        m.d.comb += [
+            self.o.valid.eq(sample_out.valid & first_fifo.r_rdy),
+            self.o.payload.sample.eq(sample_out.payload),
+            self.o.payload.first.eq(first_fifo.r_data),
+            sample_out.ready.eq(self.o.ready & first_fifo.r_rdy),
+            first_fifo.r_en.eq(self.o.valid & self.o.ready),
+        ]
+
+        return m
