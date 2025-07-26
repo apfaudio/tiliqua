be1529852468fcdadb80cfcfe0459dbff6e63fea
Sebastian Holzapfel <me@sebholzapfel.com>
2025-07-26 22:31:08 +0200

    top/xbeam: fix warning

--- gateware/src/top/xbeam/fw/src/main.rs
+++ gateware/src/top/xbeam/fw/src/main.rs
@@ -96,8 +96,6 @@ fn main() -> ! {
         let h_active = display.size().width;
         let v_active = display.size().height;
 
-        use tiliqua_hal::pmod::EurorackPmod;
-
         loop {
 
             let (opts, draw_options) = critical_section::with(|cs| {
