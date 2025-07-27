50062fdac0bab94649737f6d12e0730dd2cf50f9
Sebastian Holzapfel <me@sebholzapfel.com>
2025-07-27 14:24:14 +0200

    scripts: s/vectorscope_no_soc/spectrogram to avoid name collision

--- gateware/scripts/build_bitstreams_no_soc.sh
+++ gateware/scripts/build_bitstreams_no_soc.sh
@@ -24,7 +24,7 @@ parallel --halt now,fail=1 --jobs 0 --ungroup "{} $@" ::: \
   "pdm dsp build --dsp-core=stft_mirror" \
   "pdm dsp build --dsp-core=vocoder" \
   "pdm vectorscope_no_soc build --fs-192khz" \
-  "pdm vectorscope_no_soc build --fs-192khz --spectrogram" \
+  "pdm vectorscope_no_soc build --fs-192khz --spectrogram --name=spectrogram" \
   "pdm bootstub build" \
   "pdm usb_audio build" \
   "pdm usb_host build --midi-device=yamaha-cp73"
