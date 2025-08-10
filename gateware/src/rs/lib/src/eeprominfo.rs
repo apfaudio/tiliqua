986fdc7b103833831227e89176019a1d1f1bea8b
Sebastian Holzapfel <me@sebholzapfel.com>
2025-08-10 18:17:59 +0200

    lib/eeprom_manager: add crc to calibration constants

--- gateware/src/rs/lib/src/eeprominfo.rs
+++ gateware/src/rs/lib/src/eeprominfo.rs
@@ -1,11 +1,14 @@
 use embedded_hal::i2c::I2c;
 use serde_derive::{Serialize, Deserialize};
 use tiliqua_hal::eeprom::{EepromDriver, EepromError};
+use crc::{Crc, CRC_32_BZIP2};
+use serde;
 
 const EEPROM_CALIBRATION_ADDR: u8 = 0x00;
 const EEPROM_CALIBRATION_SIZE: usize = 0x40;
 const EEPROM_CONFIG_ADDR: u8 = 0x40;
 const EEPROM_CONFIG_SIZE: usize = 0x40;
+const CRC_ALGORITHM: Crc<u32> = Crc::<u32>::new(&CRC_32_BZIP2);
 
 #[derive(Debug, PartialEq, Clone, Serialize, Deserialize)]
 pub struct EepromCalibration {
@@ -35,35 +38,43 @@ where
         }
     }
 
-    pub fn read_calibration(&mut self) -> Result<EepromCalibration, EepromError<I2C::Error>> {
-        let mut buffer = [0u8; EEPROM_CALIBRATION_SIZE];
-        self.eeprom.read_bytes(EEPROM_CALIBRATION_ADDR, &mut buffer)?;
-        match postcard::from_bytes(&buffer) {
-            Ok(cal_data) => Ok(cal_data),
+    fn read_data<T, const SIZE: usize>(&mut self, addr: u8) -> Result<T, EepromError<I2C::Error>>
+    where
+        T: serde::de::DeserializeOwned,
+    {
+        let mut buffer = [0u8; SIZE];
+        self.eeprom.read_bytes(addr, &mut buffer)?;
+        let digest = CRC_ALGORITHM.digest();
+        match postcard::from_bytes_crc32(&buffer, digest) {
+            Ok(data) => Ok(data),
             Err(_) => Err(EepromError::InvalidData),
         }
+    }
+
+    fn write_data<T, const SIZE: usize>(&mut self, addr: u8, data: &T) -> Result<(), EepromError<I2C::Error>>
+    where
+        T: serde::Serialize,
+    {
+        let mut buffer = [0u8; SIZE];
+        let digest = CRC_ALGORITHM.digest();
+        let serialized = postcard::to_slice_crc32(data, &mut buffer, digest)
+            .map_err(|_| EepromError::InvalidSize)?;
+        self.eeprom.write_bytes(addr, serialized)
+    }
+
+    pub fn read_calibration(&mut self) -> Result<EepromCalibration, EepromError<I2C::Error>> {
+        self.read_data::<EepromCalibration, EEPROM_CALIBRATION_SIZE>(EEPROM_CALIBRATION_ADDR)
     }
 
     pub fn write_calibration(&mut self, cal_data: &EepromCalibration) -> Result<(), EepromError<I2C::Error>> {
-        let mut buffer = [0u8; EEPROM_CALIBRATION_SIZE];
-        let serialized = postcard::to_slice(cal_data, &mut buffer)
-            .map_err(|_| EepromError::InvalidSize)?;
-        self.eeprom.write_bytes(EEPROM_CALIBRATION_ADDR, serialized)
+        self.write_data::<EepromCalibration, EEPROM_CALIBRATION_SIZE>(EEPROM_CALIBRATION_ADDR, cal_data)
     }
 
     pub fn read_config(&mut self) -> Result<EepromConfig, EepromError<I2C::Error>> {
-        let mut buffer = [0u8; EEPROM_CONFIG_SIZE];
-        self.eeprom.read_bytes(EEPROM_CONFIG_ADDR, &mut buffer)?;
-        match postcard::from_bytes(&buffer) {
-            Ok(config) => Ok(config),
-            Err(_) => Err(EepromError::InvalidData),
-        }
+        self.read_data::<EepromConfig, EEPROM_CONFIG_SIZE>(EEPROM_CONFIG_ADDR)
     }
 
     pub fn write_config(&mut self, config: &EepromConfig) -> Result<(), EepromError<I2C::Error>> {
-        let mut buffer = [0u8; EEPROM_CONFIG_SIZE];
-        let serialized = postcard::to_slice(config, &mut buffer)
-            .map_err(|_| EepromError::InvalidSize)?;
-        self.eeprom.write_bytes(EEPROM_CONFIG_ADDR, serialized)
+        self.write_data::<EepromConfig, EEPROM_CONFIG_SIZE>(EEPROM_CONFIG_ADDR, config)
     }
 }
