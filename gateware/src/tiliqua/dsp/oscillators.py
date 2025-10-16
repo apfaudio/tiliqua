5c9859cf9048c67d69e82cb35f10f21e2bb89b1b
Sebastian Holzapfel <me@sebholzapfel.com>
2025-10-16 22:28:31 +0200

    add dwo

--- gateware/src/tiliqua/dsp/oscillators.py
+++ gateware/src/tiliqua/dsp/oscillators.py
@@ -56,3 +56,85 @@ class SawNCO(wiring.Component):
             m.d.sync += s.eq(s + self.i.payload.freq_inc),
 
         return m
+
+class WhiteNoise(wiring.Component):
+
+    """
+    https://www.musicdsp.org/en/latest/Synthesis/216-fast-whitenoise-generator.html
+    """
+
+    o: Out(stream.Signature(ASQ))
+
+    def elaborate(self, platform):
+        m = Module()
+
+        x0 = Signal(unsigned(32), init=0x67452301)
+        x1 = Signal(unsigned(32), init=0xefcdab89)
+
+        m.d.comb += [
+            self.o.payload.as_value().eq(x1>>(ASQ.f_bits+ASQ.i_bits)),
+        ]
+
+        with m.FSM() as fsm:
+            with m.State('X'):
+                m.d.sync += x1.eq(x1+x0)
+                m.next = 'OUT'
+            with m.State('OUT'):
+                m.d.comb += self.o.valid.eq(1),
+                with m.If(self.o.ready):
+                    m.d.sync += x0.eq(x0^x1)
+                    m.next = 'X'
+
+        return m
+
+class DWO(wiring.Component):
+
+    """
+    Digital Waveguide Oscillator.
+
+    Emits a fixed frequency and amplitude sine wave, without needing
+    a sine LUT. Instead, only uses 2 registers, 1 multiply, 3 adds.
+
+    This is not easy to modulate, but it is an interesting way to
+    achieve a high-fidelity sine wave with minimal resources.
+
+    For details, see Fig. 3 from:
+        https://ccrma.stanford.edu/~jos/wgo/Second_Order_Waveguide_Filter.html
+    """
+
+    o: Out(stream.Signature(ASQ))
+
+    def __init__(self, sq=None):
+        super().__init__()
+        self.sq = sq or self.o.payload.shape()
+
+    def elaborate(self, platform):
+        m = Module()
+
+        sq = self.sq
+
+        # Frequency tuning coefficient (-1 to +1)
+        C = fixed.Const(0.99, shape=sq)
+
+        # Initial conditions (determines sine amplitude)
+        sg1 = Signal(sq, init=fixed.Const(0.5, shape=sq))
+        o1 = Signal(sq, init=fixed.Const(0.0, shape=sq))
+
+        o = Signal(sq)
+        sg = Signal(sq)
+        with m.If(self.o.ready):
+            m.d.sync += [
+                o1.eq(o),
+                sg1.eq(sg),
+            ]
+
+        oc = Signal(sq)
+        m.d.comb += [
+            oc.eq((o1+sg1)*C),
+            sg.eq(oc+o1),
+            o.eq(oc-sg1),
+            self.o.payload.eq(sg1),
+            self.o.valid.eq(1),
+        ]
+
+        return m
