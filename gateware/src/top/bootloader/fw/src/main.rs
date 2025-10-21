509725c675112dec1c7e33adfeed179968be1aee
Sebastian Holzapfel <me@sebholzapfel.com>
2025-10-21 14:36:39 +0200

    bootloader: fix autoboot position

--- gateware/src/top/bootloader/fw/src/main.rs
+++ gateware/src/top/bootloader/fw/src/main.rs
@@ -153,7 +153,7 @@ where
     write!(countdown_text, "Autoboot {} (slot {}) in {}sec", target, slot, countdown_sec).ok();
     Text::with_alignment(
         &countdown_text,
-        Point::new(h_active/2, v_active/2 - 115),
+        Point::new(h_active/2, v_active/2 - 135),
         style,
         Alignment::Center,
     )
@@ -242,7 +242,7 @@ where
     .draw(d).ok();
     Text::with_alignment(
         "Select a bitstream. To return here, hold encoder down for 3sec.",
-        Point::new((h_active/2) as i32, (v_active/2-30) as i32 + ot),
+        Point::new((h_active/2) as i32, (v_active-180) as i32),
         norm,
         Alignment::Center,
     )
